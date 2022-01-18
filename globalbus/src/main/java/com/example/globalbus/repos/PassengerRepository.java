60c2099fb0063f84be3ac91d3e536ddd290dda76
Adil Sabir Azeez <63090923+adilsazeez@users.noreply.github.com>
2022-01-18 08:04:51 +0000

    Delete globalbus directory

--- globalbus/src/main/java/com/example/globalbus/repos/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.globalbus.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.globalbus.entities.Passenger;
-
-public interface PassengerRepository extends JpaRepository<Passenger,Long> {
-
-}
